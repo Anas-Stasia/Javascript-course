eded167f30ef57a676efca79c0058d8c7f693550
Anas-Stasia <anastasiandanwea@gmail.com>
2026-01-02 13:29:55 +0000

    New js

--- function/index.js
+++ function/index.js
@@ -6,65 +6,10 @@
 
 
 
-
-function greetUser(name){
-    return `Hello, ${name}`
-}
-greetUser("Anas")
-
-function calculateBMI(mass, height) {
-  return mass / (height ** 2);
-}
-
-calculateBMI(78, 1.69); // ≈ 27.3
-
-function calculateAge(birthYear, currentYear) {
-  return currentYear - birthYear;
-}
-
-calculateAge(2002, 2025); // 23
-
-
-// Functions
-function logger() {
-  console.log('My name is Jonas');
-}
-
-// calling / running / invoking function
-logger();
-logger();
-logger();
-
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+function fruitProcessor(mangoes,oranges){
+  console.log(mangoes, oranges)
+  const juice = `These are ${mangoes} mangoes and ${oranges} oranges`
   return juice;
 }
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
-
-const num = Number('23');
-
-
-///////////////////////////////////////
-// Function Declarations vs. Expressions
-
-// Function declaration
-function calcAge1(birthYeah) {
-  return 2037 - birthYeah;
-}
-const age1 = calcAge1(1991);
-
-// Function expression
-const calcAge2 = function (birthYeah) {
-  return 2037 - birthYeah;
-}
-const age2 = calcAge2(1991);
-
-console.log(age1, age2);
-
-
-
+fruitProcessor(5,0)
