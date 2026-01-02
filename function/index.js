bc353524b8dc8fd93a6d0b6d504ca7a27049afa8
Anas-Stasia <anastasiandanwea@gmail.com>
2026-01-02 14:57:32 +0000

    New update

--- function/index.js
+++ function/index.js
@@ -6,10 +6,38 @@
 
 
 
-function fruitProcessor(mangoes,oranges){
-  console.log(mangoes, oranges)
-  const juice = `These are ${mangoes} mangoes and ${oranges} oranges`
+function myCalculator(a,b){
+  
+  const sum = a + b
+  return `This is the summation of ${a} and ${b} :${sum}`
+}
+ const sum = myCalculator(4,5)
+ console.log(sum)
+
+ const subtract = myCalculator(9,10);
+ console.log(subtract)
+
+ // Functions
+function logger() {
+  console.log('My name is Jonas');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
   return juice;
 }
 
-fruitProcessor(5,0)
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+
+
