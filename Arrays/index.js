10b8529ec354c92d189ad5acdba120d9f235e78b
Anas-Stasia <anastasiandanwea@gmail.com>
2026-01-07 14:46:15 +0000

    Arrays

---
+++ Arrays/index.js
@@ -0,0 +1,11 @@
+let friends = ['Anas', 'Nina', 'Enoch', 'Viera']
+console.log(friends.length)
+console.log(friends[friends.length - 1])
+
+friends[3] = 'Patrick';
+
+let calcAge4 = (birthYear) => 2037 - birthYear;
+
+const years = [1990,2000,20004,2006,2009,2001];
+let age1 = calcAge4(years[0])
+console.log(age1)
