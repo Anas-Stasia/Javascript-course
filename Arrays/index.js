d46855ac2497c269bb2757f84ad3ccbd4c1a6d11
Anas-Stasia <anastasiandanwea@gmail.com>
2026-01-08 14:01:24 +0000

    New update

--- Arrays/index.js
+++ Arrays/index.js
@@ -81,6 +81,18 @@ const total = [
     bills[1] + tips[1],
     bills[2] + tips[3]
 ]
-console.log(bills);
-console.log(total)
-console.log(tips)
+console.log(bills, total, tips);
+
+
+
+
+const calcTip1 = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills1 = [125, 555, 44];
+const tips1 = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
