04849882c6be3edcdbf7a60e585535e1a728b188
Anas-Stasia <anastasiandanwea@gmail.com>
2026-01-08 16:35:58 +0000

    Still on arrays

---
+++ Objects/index.js
@@ -0,0 +1,37 @@
+let anas = {
+    firstName:'Anastasia',
+    lastName:'Ndanwea',
+    age:2037 - 1991,
+    job: 'Student',
+    friends: ['Alex', 'Jonas', 'Adwoa', 'Viera']
+
+}
+console.log(anas.firstName)
+console.log(anas['lastName'])
+
+const nameKey = 'Name';
+console.log(anas['first' + nameKey]);
+console.log(anas['last' + nameKey]);
+
+// console.log(anas.'last' + nameKey)
+
+const interestedIn = prompt('What do you want to know about Anas? Choose between firstName, lastName,age,job, and friends');
+
+if(anas[interestedIn]){
+    console.log(anas[interestedIn])
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job, and friends')
+}
+console.log(anas[interestedIn])
+
+anas.location = 'Ghana';
+anas['twitter'] = '@Anastasia';
+console.log(anas);
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Michael"
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
+
+console.log(`${anas.firstName} has 3 friends: ${anas.friends[0]},${anas.friends[1]}, ${anas.friends[2]} and his best friend is called ${anas.friends[3]} `)
+
+console.log(`${anas.firstName} has ${anas.friends.length} friends and his best friend is called ${anas.friends[3]} `)
