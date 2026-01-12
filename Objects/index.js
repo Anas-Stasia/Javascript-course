692c9150c2b72510546707765f98cef88de3cf91
Anas-Stasia <anastasiandanwea@gmail.com>
2026-01-12 15:55:12 +0000

    New update

--- Objects/index.js
+++ Objects/index.js
@@ -115,7 +115,7 @@ calcAge: function () {
 },
 
   getSummary : function() {
-    return `${this.firstName} is a ${this.calcAge()} year old ${anas.jobjob} and he has a driver's ${hasDriversLicense ? 'a' : 'no'}`
+    return `${this.firstName} is a ${this.calcAge()} year old ${anas.jobjob} and he has  ${hasDriversLicense ? 'a' : 'no'} driver's license.`
   }
 }
    console.log(anas.age)
@@ -124,6 +124,46 @@ calcAge: function () {
 //Challenge
 // Jonas is a 46-year old teacher, and he has driver's license
 
+// Let's go back to Mark and John comparing their BMIs! This time, let's use objects to 
+// implement the calculations! Remember: BMI = mass / height ** 2 = mass 
+// / (height * height) (mass in kg and height in meter)
+// Your tasks:
+// 1. For each of them, create an object with properties for their full name, mass, and 
+// height (Mark Miller and John Smith)
+// 2. Create a 'calcBMI' method on each object to calculate the BMI (the same 
+// method on both objects). Store the BMI value to a property, and also return it 
+// from the method
+// 3. Log to the console who has the higher BMI, together with the full name and the 
+// respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
 
 
+const mark = {
+    fullName:'Mark Miller',
+    mass:'78',
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height ** 2);
+        return this.bmi;
+    }
+};
 
+const john = {
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height ** 2);
+    return this.bmi;
+  }
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+console.log(mark.bmi, john.bmi);
+
+if(mark.bmi >= john.bmi){
+    console.log(`${mark.fullName} BMI ${mark.bmi} is higher than John's(${john.bmi})`)
+}else {
+  console.log(`john.fullName BMI ${john.bmi} is higher than Mark's(${mark.bmi})`)  
+}
