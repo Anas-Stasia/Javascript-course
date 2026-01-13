2db3d6ef04b41dfcc7bb91e5325a19bfe079d9ec
Anas-Stasia <anastasiandanwea@gmail.com>
2026-01-13 21:08:19 +0000

    New for loop

--- Objects/index.js
+++ Objects/index.js
@@ -170,4 +170,29 @@ const john = {
     `${john.fullName}'s BMI (${john.bmi.toFixed(1)}) is higher than ${mark.fullName}'s (${mark.bmi.toFixed(1)})!`
   );
 }
+
+// Looping Backwards and Loops in Loops
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true
+];
+
+// 0, 1, ..., 4
+// 4, 3, ..., 0
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`-------- Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
+  }
+}
  
