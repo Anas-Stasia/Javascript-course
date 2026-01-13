1b131e8f2ef8b7a40b7930c89b5c44beab7a4b0e
Anas-Stasia <anastasiandanwea@gmail.com>
2026-01-13 09:08:04 +0000

    New update

--- Objects/index.js
+++ Objects/index.js
@@ -138,32 +138,26 @@ calcAge: function () {
 
 
 const mark = {
-    fullName:'Mark Miller',
-    mass:'78',
-    height: 1.69,
-    calcBMI: function () {
-        this.bmi = this.mass / (this.height ** 2);
-        return this.bmi;
-    }
-};
+    fullName: 'Mark Miller',
+    mass: 78,
+    height:1.69,
+
+     calcBMI:  function(){
+    this.bmi = this.mass/this.height **2
+    return this.bmi
+}
+}
 
 const john = {
-  fullName: 'John Smith',
-  mass: 92,
-  height: 1.95,
-  calcBMI: function () {
-    this.bmi = this.mass / (this.height ** 2);
-    return this.bmi;
-  }
-};
+    fullName: 'John Smith',
+    mass: 92,
+    height:1.95,
 
-mark.calcBMI();
-john.calcBMI();
-
-console.log(mark.bmi, john.bmi);
-
-if(mark.bmi >= john.bmi){
-    console.log(`${mark.fullName} BMI ${mark.bmi} is higher than John's(${john.bmi})`)
-}else {
-  console.log(`john.fullName BMI ${john.bmi} is higher than Mark's(${mark.bmi})`)  
+    calcBMI:  function(){
+    this.bmi = this.mass/this.height **2
 }
+}
+   mark.calcBMI();
+   john.calcBMI();
+   console.log(mark.bmi,john.bmi)
+ 
