5c05dd266e2eae29e304c01d099f1631ba841d65
Anas-Stasia <anastasiandanwea@gmail.com>
2026-01-29 19:05:19 +0000

    Update

--- whileLoop/index.js
+++ whileLoop/index.js
@@ -8,3 +8,22 @@ while (age === undefined || age < 0 || age > 120) {
     }
 }
 console.log(`Valid age entered: ${age}`);
+
+
+// Process array elements
+let numbers = [5, 12, 8, 20, 3, 15];
+let i = 0;
+let sum = 0;
+
+while (i < numbers.length) {
+    sum += numbers[i];
+    i++;
+}
+console.log(`Sum: ${sum}`);
+
+// Find first number divisible by 7
+let num = 1;
+while (num % 7 !== 0 || num === 0) {
+    num = Math.floor(Math.random() * 100) + 1;
+}
+console.log(`Found number divisible by 7: ${num}`);
